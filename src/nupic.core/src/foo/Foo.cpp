e624e189cee6ad34d37e1609f78e5ea565e9d84d
mattheiler <heiler.matthewd@gmail.com>
2017-12-14 10:34:54 -0600

    Added Yaml-Cpp and ZLib.

--- src/nupic.core/src/foo/Foo.cpp
+++ src/nupic.core/src/foo/Foo.cpp
@@ -1,7 +1,13 @@
 #include <nupic/foo/Foo.hpp>
 
+#include <boost/any.hpp>
+
 #include <capnp/message.h>
 #include <capnp/serialize-packed.h>
+
+#include <yaml-cpp\binary.h>
+
+#include <zlib.h>
 
 Foo::Foo(void)
 {
